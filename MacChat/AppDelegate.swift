ae0b2138c57a6c1a0c37c6ce84db9a9bd555b3c5
Russell Brown <russell.brown67@googlemail.com>
2017-12-24 22:10:15 +0000

    Initial Commit

---
+++ MacChat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacChat
+//
+//  Created by Russell Brown on 24/12/2017.
+//  Copyright © 2017 Russell Brown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacChat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacChat
+//
+//  Created by Russell Brown on 24/12/2017.
+//  Copyright © 2017 Russell Brown. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
